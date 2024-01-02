48cd4560b59923a85afaf4c7ae820c5219d636e2
Kunal Singh <kunalsin9h@gmail.com>
2024-01-02 16:28:20 +0530

    add funding.yaml

--- cmd/api/cors.go
+++ cmd/api/cors.go
@@ -7,4 +7,5 @@ EnableCors - Enable CORS for the given response writer
 */
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "https://i.imgur.com")
+	(*w).Header().Set("Access-Control-Allow-Origin", "https://tiddi.kunalsin9h.com")
 }
